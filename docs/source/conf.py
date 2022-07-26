5952321309f8c3094ee81fa8b99d518e5afee8b5
Olivier Barais <olivieR.barais@gmail.com>
2022-07-26 11:12:45 +0200

    update dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,8 @@ release = 'latest'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+#extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
